1d4dbaf006e7b1c99c7367b82e3f77b6a91c5874
26583 <43673592+26583@users.noreply.github.com>
2020-02-28 09:18:40 +0100

    Add files via upload

---
+++ PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 100;
+    [SerializeField]
+    Text healthCounter;
+    
+    void Start()
+    {
+        RefreshHealthCounter();
+    }
+    void OnMouseDown()
+    {
+        health -= 10;
+        Debug.Log(health);
+        if(health < 0)
+        {
+            health = 0;
+            Destroy(gameObject);
+        }
+        RefreshHealthCounter();
+    }
+    void RefreshHealthCounter()
+    {
+        healthCounter.text = "Health: " + health;
+    }
+}
