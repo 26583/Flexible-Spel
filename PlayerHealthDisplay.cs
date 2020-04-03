ef08c57a88cc5658f1935314f53eda658328b233
26583 <43673592+26583@users.noreply.github.com>
2020-04-03 11:29:22 +0200

    Add files via upload

---
+++ PlayerHealthDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthDisplay : MonoBehaviour
+{
+    [SerializeField]
+    Text healthCounter;
+    PlayerHealth healthClass;
+    void Start()
+    {
+        healthClass = gameObject.GetComponent<PlayerHealth>();
+        RefreshHealthCounter();
+    }
+    public void RefreshHealthCounter()
+    {
+        healthCounter.text = "Health: " + healthClass.GetHealth();
+    }
+}
